08f928546d9d73b9c08f87e144896d6946ed84e6
Mehedi Hasan <u1904067@student.cuet.ac.bd>
2025-11-27 18:15:32 +0600

    first commit

---
+++ dist/jsDunia.js
@@ -0,0 +1,6 @@
+const friends = ['Rahim', 'Karim'];
+const schoolFriend = ['Pintu', 'Chintu', 'Bulbul'];
+const collegeFriend = ['Mr. Smart','Mr very very smart'];
+
+friends.push(schoolFriend); //[ 'Rahim', 'Karim', [ 'Pintu', 'Chintu', 'Bulbul' ] ]
+console.log(friends)
